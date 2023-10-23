dc240eec39acd165715f7b94bdaf35c47fdb3c5a
cat <macy.cordner@gmail.com>
2023-10-22 20:50:55 -0700

    smalll changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,9 @@
 const About = () => {
     return (
-        <div id="about">
-
+        <div id="about" className="mb-[200px]">
+            <h2 className="ml-2 mb-3 text-xl">About Me</h2>
+            <p>Hello! I'm Cat, a recent grad from the Front-End Web Development Program at BCIT.
+                As a kid, my dream job was to be a video-game designer (though now i'd rather just play them) so I knew i wanted to do something in tech as a career. I also have past experience with some back-end languages like C++ and Java. In my free-time, I really like to game (which i like to think has helped my ability to work with a team).</p>
         </div>
     );
 };
