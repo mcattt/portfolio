b48c200575e7cae6d1244c9987f558177c186c40
cat <macy.cordner@gmail.com>
2023-11-02 21:48:57 -0700

    finally fixed accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,8 +4,8 @@ const Accordion = ({ title, content }) => {
     const [isActive, setIsActive] = useState(false);
 
     return (
-        <div className='' >
-            <div className='flex flex-row items-center justify-between ' >
+        <div className='whitespace-normal' >
+            <div className='flex flex-row items-center justify-between' >
                 <div className='  font-medium'>{title}</div>
                 <div onClick={() => setIsActive(!isActive)} className={`ml-2 w-4 transition-all duration-700 ease-in-out ${isActive ? ' rotate-180' : ''}`}> <img src={downArrow} alt="" /> </div>
             </div>
